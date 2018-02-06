2dbe87436607fb43d882347d430cb914ba67918b
usopp <usopp9@naver.com>
2018-02-06 10:42:23 +0900

    db 추가

---
+++ Scripts/Script.sql
@@ -0,0 +1,18 @@
+create schema JDBC_Mybatis;
+-- 학생
+CREATE TABLE student (
+	stud_id INTEGER     NOT NULL COMMENT '학번',
+	name    VARCHAR(50) NOT NULL COMMENT '성명',
+	email   VARCHAR(40) NOT NULL COMMENT '메일',
+	dob     DATE        NULL     COMMENT '입학일자'
+)
+COMMENT '학생';
+
+-- 학생
+ALTER TABLE student
+	ADD CONSTRAINT PK_Student
+		PRIMARY KEY (
+			stud_id 
+		);
+
+show tables;
